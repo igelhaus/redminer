2886b4df10b2f0cde57cf51d02afe54f76f34e95
Anton Soldatov <igelhaus@gmail.com>
2014-04-17 01:52:16 +0400

    * Added initial version of batch-grant action

---
+++ Redminer/Command/BatchGrant.pm
@@ -0,0 +1,99 @@
+package Redminer::Command;
+
+use 5.010;
+use strict;
+use warnings;
+
+sub _args_spec { [ 'project=s@', 'user=s@', 'group=s@', 'role=s@', ] }
+
+sub _run
+{
+	my $self = shift;
+
+	my @role_ids;
+	$self->iterate('roles', sub {
+		my $role = shift;
+		return if !$self->filter('role', $role, {
+			regex => [qw/name/],
+			plain => [qw/name id/],
+		});
+		push @role_ids, $role->{id};
+		$self->log(sprintf 'Role \'%s\' (internal ID %d) will be granted',
+			Encode::encode_utf8($role->{name}),
+			$role->{id},
+		);
+	});
+	if (@role_ids == 0) {
+		$self->log('--role filter not specified');
+		return;
+	}
+
+	my @projects;
+	$self->iterate('projects', sub {
+		my $project = shift;
+		return if !$self->filter('project', $project, {
+			regex => [qw/identifier name/],
+			plain => [qw/identifier id/],
+		});
+		push @projects, $project;
+	});
+
+	# FIXME: fetch groups
+	my @groups;
+	$self->iterate('groups', sub {
+		my $group = shift;
+		return if !$self->filter('group', $group, {
+			regex => [qw/name/],
+			plain => [qw/name id/],
+		});
+		push @groups, $group;
+	});
+
+	my @users;
+	$self->iterate('users', sub {
+		my $user = shift;
+		return if !$self->filter('user', $user, {
+			regex => [qw/login firstname lastname/],
+			plain => [qw/login firstname lastname id/],
+		});
+		push @users, $user;
+	});
+
+	if (@groups == 0 && @users == 0) {
+		$self->log('Neither --group nor --user filters specified');
+		return;
+	}
+
+	foreach my $project (@projects) {
+		$self->log(sprintf 'Processing project \'%s\' (internal ID %d)...',
+			Encode::encode_utf8($project->{name}),
+			$project->{id},
+		);
+		foreach my $group (@groups) {
+			$self->log(sprintf "\tGranting to group '%s' (internal ID %d)",
+				Encode::encode_utf8($group->{name}),
+				$group->{id},
+			);
+			$self->engine->createProjectMembership($project->{id}, {
+				user_id  => $group->{id},
+				role_ids => \@role_ids,
+			});
+		}
+		foreach my $user (@users) {
+			$self->log(sprintf "\tGranting to user '%s %s' (internal ID %d)",
+				Encode::encode_utf8($user->{firstname}),
+				Encode::encode_utf8($user->{lastname}),
+				$user->{id},
+			);
+			$self->engine->createProjectMembership($project->{id}, {
+				user_id  => $user->{id},
+				role_ids => \@role_ids,
+			});
+		}
+		$self->log('Project processed');
+	}
+
+	return 1;
+}
+
+1;
